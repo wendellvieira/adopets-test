9d37dd18c8d6d48e30301d009cc41967dcfa1a40
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-01-02 09:03:07 -0300

    Update primary color

--- config-overrides.js
+++ config-overrides.js
@@ -9,7 +9,7 @@ module.exports = override(
     addLessLoader({
         javascriptEnabled: true,
         modifyVars: {
-            "@primary-color": "#1890ff", // primary color for all components
+            "@primary-color": "#ce3f71", // primary color for all components
             "@link-color": "#1890ff", // link color
             "@success-color": "#52c41a", // success state color
             "@warning-color": "#faad14", // warning state color
