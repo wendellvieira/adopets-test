38c7c5b0c76f7b31284158e3b8e0375eb2017424
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-01-02 17:53:28 -0300

    fix filter using enum

--- src/pages/search/components/filters.tsx
+++ src/pages/search/components/filters.tsx
@@ -1,45 +1,56 @@
 import React from "react"
 import { Select, Radio, Button, Row, Col } from 'antd';
+import {iFilter, sex_key, size_key, age_key} from "../index";
 
-const size:object  = { S:"SMALL", M: "MEDIUM", L: "LARGE", XL: "EXTRA LARGE" }
-const age:string[] = [ "BABY", "YOUNG", "ADULT", "SENIOR" ]
+const { Option } = Select;
+interface iProps {
+    filters?: iFilter,
+    setFilter: (filter:iFilter ) => void,
+    refreshList: () => void
+}
 
-const Filters: React.FC = () => {
-
-    const mapOptions = ( options:string[]  ) => {
-        return options.map( ( item:string, index:number ) => (
-            <Select.Option key={index} value={item}>
-                {item}
-            </Select.Option>
-        ))
-    }
-    
+const Filters: React.FC<iProps> = ( props:iProps ) => {
 
     return (
         <React.Fragment>
             <Row type="flex" justify="start" gutter={[16, 32]}>
                 <Col>
-                    <Radio.Group>
-                        <Radio.Button value="MALE">MALE</Radio.Button>
-                        <Radio.Button value="FEMALE">FEMALE</Radio.Button>
+                    <Radio.Group 
+                        value={(props.filters||{}).sex_key}
+                        onChange={ ev => props.setFilter({ sex_key: ev.target.value }) }
+                    >
+                        <Radio.Button value={sex_key.MALE}>MALE</Radio.Button>
+                        <Radio.Button value={sex_key.FEMALE}>FEMALE</Radio.Button>
                     </Radio.Group>
                 </Col>
                 <Col>
                     <Select 
+                        value={(props.filters||{}).size_key}
+                        onChange={ (size_key:number | null ) => props.setFilter({size_key}) }
                         style={{ width: 150 }}
                         placeholder="SELECT A SIZE">
-                        {mapOptions( Object.values(size) )}
+                            <Option value={size_key.ALL}>ALL</Option>
+                            <Option value={size_key.S}>SMALL</Option>
+                            <Option value={size_key.M}>MEDIUM</Option>
+                            <Option value={size_key.L}>LARGE</Option>
+                            <Option value={size_key.XL}>EXTRA LARGE</Option>
                     </Select>
                 </Col>
                 <Col>
                     <Select 
+                        value={(props.filters||{}).age_key}
+                        onChange={ (age_key:number | null ) => props.setFilter({age_key}) }
                         style={{ width: 150 }}
                         placeholder="SELECT A AGE">
-                        {mapOptions( age )}
+                            <Option value={age_key.ALL}>ALL</Option>
+                            <Option value={age_key.BABY}>BABY</Option>
+                            <Option value={age_key.YOUNG}>YOUNG</Option>
+                            <Option value={age_key.ADULT}>ADULT</Option>
+                            <Option value={age_key.SENIOR}>SENIOR</Option>
                     </Select>
                 </Col>
                 <Col>
-                    <Button type="primary" icon="heart">
+                    <Button type="primary" onClick={props.refreshList} icon="heart">
                         FIND MY PET
                     </Button>
                 </Col>
